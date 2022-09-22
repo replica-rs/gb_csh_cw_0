e25648c4de9fd7ebe0156c5f05ffea57ac8403a1
replica-rs <pochernikov99@mail.ru>
2022-09-22 16:07:42 +0300

    prog

---
+++ program/Program.cs
@@ -0,0 +1,58 @@
+﻿string[] GetStrings()
+{
+    Console.WriteLine("Введите несколько строк через запятую (Just,Johnny):");
+    string value = Convert.ToString(Console.ReadLine());
+    string[] temp = value.Split(',');
+
+    return temp;
+}
+
+string[] ArraySo3CH( string[] value)
+{
+    int count = 0;
+    for (int i = 0; i < value.Length; i++) 
+    {
+        if (value[i].Length <= 3) 
+        {
+            count = count + 1;
+        }
+    }
+    
+    if (count > 0) 
+    {
+        string[] temp = new string[count];
+
+        for (int i = 0; i < value.Length; i++) 
+    {
+        if (value[i].Length <= 3) 
+        {
+            temp[i] = value[i];
+        }
+    }
+
+        return temp;
+    } 
+    else 
+    {
+        string[] temp = new string[] {""};
+        System.Console.WriteLine("Все строки в массиве имеют более 3-х символов.");
+
+        return temp;
+        
+    }
+}
+
+void WriteArray(string[] value)
+{
+    int count = 0;
+    while(count < value.Length)
+    {
+        System.Console.Write($"{value[count]} ");
+        count = count + 1;
+    }
+    System.Console.WriteLine("");
+}
+
+
+
+WriteArray(ArraySo3CH(GetStrings()));
